a1b254d816f64583755be64eea7c8896f7bebf72
PreakPro <shaurya.preak1239@outlook.com>
2025-06-15 20:03:25 +0530

    first commit, but game is technically already over

--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,214 @@
-import Image from "next/image";
+"use client"
+import { languages } from "../lib/languages"
+import { getFarewellText, cn } from "../lib/utils"
+import { useEffect, useMemo, useRef, useState } from "react"
+import { words } from "../lib/words"	
+import { motion } from "framer-motion"
+import confetti from "canvas-confetti"
+ 
+export default function AssemblyEndgame() {
+	const [currentWord, setCurrentWord] = useState<string>("")
+	const [guessedLetters, setGuessedLetters] = useState<string[]>([])
+	const [isGameOver, setIsGameOver] = useState<boolean>(false)
+	const letterRefs = useRef<{[key: string]: HTMLButtonElement | null}>({})
+	const newGameRef = useRef<HTMLButtonElement>(null)
+	const alphabet = "abcdefghijklmnopqrstuvwxyz"
 
-export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+	const wrongGuesses = useMemo(() => {
+		return guessedLetters.filter(letter => !currentWord.includes(letter)).length
+	}, [guessedLetters, currentWord])
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+	useEffect(() => {
+		function generateRandomWord() {
+			const word = words[Math.floor(Math.random() * words.length)]
+			setCurrentWord(word)
+		}
+		generateRandomWord()
+	}, [])
+
+	const languagesDisplay = languages.map((values, key) => (
+		<div key={key} className="relative inline-block justify-center items-center text-center">
+			<button
+				style={{
+					backgroundColor: values.backgroundColor,
+					color: values.color,
+				}}
+				className={cn(
+					"rounded px-3 py-1 m-1 text-black",
+					{ "opacity-30": wrongGuesses > key }
+				)}
+			>
+				{values.name}
+			</button>
+			<span className="absolute inset-0 z-10 h-full w-full translate-y-2">{wrongGuesses > key ? "💀" : null}</span>
+		</div>
+	))
+
+	function onLetterClick(letter: string, key: number) {
+		setGuessedLetters(prevLetters => prevLetters.includes(letter) ? prevLetters : [...prevLetters, letter])
+	}
+
+	const letterDisplay = (currentWord ?? "").split('').map((letter, key) => (
+		<span
+			key={key}
+			className="flex rounded-sm bg-neutral-700 h-13 w-13 m-1 select-none text-white text-xl text-center items-center justify-center border-b-neutral-300 border-b-1"
+		>
+			{guessedLetters.includes(letter) ? letter.toUpperCase() : null}
+		</span>
+	))
+
+	useEffect(() => {
+  		const handleKeyDown = (e: KeyboardEvent) => {
+			window.addEventListener("keydown", handleKeyDown);
+		}
+  		return () => {
+    		window.removeEventListener("keydown", handleKeyDown);
+  		};
+	}, []);
+
+	const keyboardDisplay = alphabet.split("").map((letter, key) => {
+		const isGuessed = guessedLetters.includes(letter)
+		const isCorrect = isGuessed && currentWord.includes(letter)
+		const isWrong = isGuessed && !currentWord.includes(letter)
+
+		return (
+			<button
+				key={key}
+				ref={(el) => {letterRefs.current[letter] = el}}
+				disabled={isGameOver}
+				onClick={() => onLetterClick(letter, key)}
+				className={cn(
+					"flex transition-all text-center items-center justify-center rounded m-1 border-1 border-white w-10 h-10 text-black",
+					{
+						"bg-amber-400 hover:bg-amber-300 hover:scale-105": !isGuessed,
+						"bg-amber-400 opacity-30": isGameOver,
+						"bg-green-500": isCorrect,
+						"bg-red-600": isWrong
+					}
+				)}
+			>
+				{letter.toUpperCase()}
+			</button>
+		)
+	})
+
+	useEffect(() => {
+		if (!currentWord) return
+
+		const hasWon = currentWord.split("").every(letter => guessedLetters.includes(letter))
+		const hasLost = wrongGuesses >= languages.length - 1
+
+		if (hasWon || hasLost) {
+			setIsGameOver(true)
+			console.log("game over desu")
+		}
+
+		if (hasWon) {
+			confetti({
+        		particleCount: 150,
+        		spread: 130,
+        		origin: { y: 0.6 }
+      });
+		}
+	}, [guessedLetters, wrongGuesses, currentWord])
+
+	useEffect(() => {
+		const handleKeyDown = (e: KeyboardEvent) => {
+			const key = e.key.toLowerCase(); 
+			const button = letterRefs.current[key];
+			if (alphabet.includes(key) && button) {
+    			button.click();
+    		}
+			if (key === "enter" || key === " ") {
+				newGameRef.current?.click()
+			}
+		}
+		window.addEventListener("keydown", handleKeyDown);
+  		return () => window.removeEventListener("keydown", handleKeyDown);
+	}, [])
+
+	function newGame() {
+		setIsGameOver(false)
+		setGuessedLetters([])
+		setCurrentWord(words[Math.floor(Math.random() * words.length)])
+	}
+
+	const farewellText = useMemo(() => {
+		if (languages[wrongGuesses - 1]) return getFarewellText(languages[wrongGuesses - 1].name)
+		return ""
+	}, [wrongGuesses])
+
+	if (!currentWord) {
+		return <main className="text-white p-10 text-center">Loading word...</main>
+	}
+
+	const hasWon = currentWord.split("").every(letter => guessedLetters.includes(letter))
+
+	console.log(currentWord)
+
+	return (
+		<div className="relative h-screen bg-neutral-800">
+			<main className={cn(
+				"flex flex-col h-full transition-all duration-300",
+				{ "blur-md pointer-events-none": isGameOver }
+			)}>
+				<header className="p-5 justify-center text-center">
+					<h1 className="text-white text-3xl">Assembly: Endgame</h1>
+					<p className="text-neutral-300 text-lg mt-2">
+						Guess the word within 8 attempts to keep the programming world safe from Assembly!
+					</p>
+				</header>
+
+				<section className={cn(
+					"rounded justify-center mx-auto text-center w-120 p-3 text-white mt-3",
+					{ "bg-red-500": wrongGuesses >= languages.length - 1 },
+					{ "bg-purple-700 p-6": wrongGuesses !== 0 && !isGameOver && wrongGuesses < languages.length - 1 }
+				)}>
+					<h2 className="text-xl font-bold">
+						{!isGameOver ? farewellText : ""}
+					</h2>
+				</section>
+
+				<section className="flex flex-wrap justify-center items-center mx-auto w-120 mt-7">
+					{languagesDisplay}
+				</section>
+
+				<section className="flex flex-row justify-center mt-4">
+					{letterDisplay}
+				</section>
+
+				<section className="flex flex-wrap w-110 justify-center text-center mx-auto m-5">
+					{keyboardDisplay}
+				</section>
+
+			</main>
+
+		{isGameOver && (
+		<div className="absolute inset-0 flex flex-col items-center justify-center z-50">
+			{hasWon ? (
+				<motion.div
+					initial={{ scale: 0.8, opacity: 0 }}
+					animate={{ scale: 1, opacity: 1 }}
+					transition={{ duration: 0.5, ease: "easeOut" }}
+					className="rounded border-2 border-neutral-300 text-white text-center mb-20 w-120 p-6 shadow-2xl bg-green-600 scale-125"
+				>
+					<h2 className="text-4xl font-bold mb-2">You Win!</h2>
+					<p className="text-lg">Well done! 🎉</p>
+				</motion.div>
+			) : (
+				<motion.div
+					initial={{ opacity: 0 }}
+					animate={{ opacity: [0.2, 1, 0.5, 1] }}
+					transition={{ duration: 1.5, repeat: Infinity, repeatType: "mirror" }}
+					className="rounded border-2 border-neutral-300 text-white text-center mb-20 w-120 p-6 shadow-2xl bg-red-700 scale-125 animate-glitch"
+				>
+					<h2 className="text-4xl font-bold mb-2 animate-glitch">YOU LOSE!</h2>
+					<p className="text-lg animate-glitch">Better start learning Assembly😭</p>
+				</motion.div>
+			)}
+				<button ref={newGameRef} onClick={newGame} className="transition-all bg-blue-400 hover:bg-blue-300 hover:scale-105 border-1 text-black border-neutral-200 rounded h-10 w-80 block mx-auto">New Game</button>
+			</div>
+			)}
+		</div>
+	)
 }
